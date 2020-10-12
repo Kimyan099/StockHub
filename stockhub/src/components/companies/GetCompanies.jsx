76c1b984b2b5aaaa1e32d71f3b3a6995aa717ccc
Getulus <tamasgaj@gmail.com>
2020-10-12 21:38:33 +0200

    nodes

---
+++ stockhub/src/components/companies/GetCompanies.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState, createContext } from "react";
+import axios from "axios";
+
+export const CompaniesContext = createContext();
+
+export const CompanyProvider = (props) => {
+  const [companies, setCompanies] = useState([]);
+
+  const access_token =
+    "bu21mlf48v6u9tetnbt0";
+
+  useEffect(() => {
+    axios
+      .get(`https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${access_token}`)
+      .then((res) => {
+        setCompanies(res.data.slice(80,1000));
+        console.log(res.data)
+      });
+  }, []);
+
+  return (
+    <CompaniesContext.Provider value={[companies, setCompanies]}>
+      {props.children}
+    </CompaniesContext.Provider>
+  );
+};
