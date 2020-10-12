f3cd15fd6c67aa4a5d0f3445ed03a29a085edb84
Bence <borsi92@gmail.com>
2020-10-12 20:23:33 +0200

    stock info is received based on stock symbol

--- stockhub/src/components/StockContext.js
+++ stockhub/src/components/StockContext.js
@@ -3,7 +3,7 @@ import React,  {useState, createContext} from 'react'
 export const StockContext = createContext();
 
 export const StockData = props => {
-    const [stock, changeStock] = useState('aapl')
+    const [stock, changeStock] = useState('AAPL')
     return (
         <StockContext.Provider value={[stock, changeStock]}>
             {props.children}
--- stockhub/src/components/Stocks.js
+++ stockhub/src/components/Stocks.js
@@ -8,28 +8,20 @@ const Stocks = props => {
     
     const [stocks, addStock] = useState([])
 
-    const stock = useContext(StockContext)
-    // const finnhub = require('finnhub');
- 
-    // const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-    // api_key.apiKey = "bu247pn48v6uohspt4jg" 
-    // const finnhubClient = new finnhub.DefaultApi()
+    const [stock] = useContext(StockContext)
 
-    // finnhubClient.stockSymbols("US", (error, data, response) => {
-    //     console.log(data[100])
-    // });
 
     useEffect (() => {
         console.log("Axios call")
-        axios.get("https://finnhub.io/api/v1/quote?symbol=AAPL&token=bu247pn48v6uohspt4jg")
-        .then(res => addStock(res.data.c))
-      },[])
+        axios.get(`https://finnhub.io/api/v1/quote?symbol=${stock}&token=bu247pn48v6uohspt4jg`)
+        .then(res => console.log(res.data.c))
+      })
 
 
     return (
         <div>
         <StockSearchBar></StockSearchBar>
-        <h2>{stock}</h2>
+        <h2>Price of {stock} is {stocks}</h2>
         </div>
     )
 }
