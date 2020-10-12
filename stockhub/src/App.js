114ff0807f236fc0cd60af85b52175991212f7fb
Getulus <tamasgaj@gmail.com>
2020-10-12 21:37:42 +0200

    companies added to app

--- stockhub/src/App.js
+++ stockhub/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/ui/navbar/NavBar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import CompanyFinder from './components/companies/Companies';
+import {CompanyProvider} from './components/companies/GetCompanies'
 
 function App() {
   return (
@@ -9,10 +11,13 @@ function App() {
 			<div id='body'>
 				<NavBar />
 				<Switch>
-					<Route path="/companies" exact></Route>
+					<CompanyProvider>
+						<Route path="/companies" render={(props) => <CompanyFinder />}>
+						</Route>
+					</CompanyProvider>
 					<Route path="/market-news" exact ></Route>
 					<Route path="/stocks" exact></Route>
-          <Route path="/profile" exact ></Route>
+          			<Route path="/profile" exact ></Route>
 				</Switch>
 			</div>
 		</Router>
