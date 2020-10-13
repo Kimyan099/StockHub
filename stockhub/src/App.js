9c652a3f3d54b756d1556425cb110aa450e2ac48
Adam Szemere <adam.szemere.96@gmail.com>
2020-10-13 18:36:47 +0200

    changed files

--- stockhub/src/App.js
+++ stockhub/src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import AdvancedNavBar from './components/NavBar';
+
+import Home from './components/pages/HomePage/Home'
 
 function App() {
   return (
     <Router>
 			<div id='body'>
 				{/* <NavBar /> */}
-        <AdvancedNavBar />
+        <NavBar />
 				<Switch>
 					<Route path='/' exact></Route>
 					<Route path="/companies" exact></Route>
@@ -17,6 +18,8 @@ function App() {
 					<Route path="/stocks" exact></Route>
           			<Route path="/profile" exact ></Route>
 				</Switch>
+
+				<Home />
 			</div>
 		</Router>
   );
