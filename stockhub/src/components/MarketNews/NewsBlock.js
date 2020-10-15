7ae2ab481d11f4b9a23e41dcef58364fbd09ef59
Kristof910 <kristof910@icloud.com>
2020-10-15 11:53:15 +0300

    bootstrap bug fixed

--- stockhub/src/components/MarketNews/NewsBlock.js
+++ stockhub/src/components/MarketNews/NewsBlock.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import './NewsBlock.css';
 import { Link } from 'react-router-dom';
 
@@ -9,7 +9,7 @@ const NewsBlock = (props) => {
 
   return (
     <div>
-      <a href={props.url} target='_blank'>
+      <a href={props.url} style={{ textDecoration: 'none' }} target='_blank'>
         <div className='card-block'>
           <Card style={{ width: '18rem' }}>
             <Card.Img variant='top' src={props.image} />
