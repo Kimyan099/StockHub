0610afb241dd85b36ff5d5f06b2bf8bd4f49629d
Adam Szemere <sephxdnb@gmail.com>
2020-10-18 11:49:33 +0200

    Home page

---
+++ stockhub/src/components/pages/HomePage/Home.jsx
@@ -0,0 +1,16 @@
+import React, {useEffect} from 'react';
+import HeroSection from './HeroSection';
+import { homeObjOne } from './Data';
+
+function Home() {
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, []) 
+  return (
+    <>
+      <HeroSection {...homeObjOne} />
+    </>
+  );
+}
+export default Home;
