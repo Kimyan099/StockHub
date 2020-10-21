4893e4c8f66d57c758da81b4992223e92284c872
Adam Szemere <sephxdnb@gmail.com>
2020-10-21 12:42:49 +0200

    another code remove

--- stockhub/src/components/pages/HomePage/Data.jsx
+++ stockhub/src/components/pages/HomePage/Data.jsx
@@ -6,8 +6,7 @@ export const homeObjOne = {
     headline: 'Unlimited Transactions with zero fees',
     description:
       'Get all the latest news, companies and stock trades with visulized grafs and details via real-time data, in one place',
-    buttonLabel: 'Get Started',
-    imgStart: '',
+    buttonLabel: 'Get Started'
 
   };
   
