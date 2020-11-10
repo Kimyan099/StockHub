db3966ef9cdd5a36306249fab7a751ff9db30ac4
Adam Szemere <sephxdnb@gmail.com>
2020-11-10 11:53:31 +0100

    stock buy not done yet

--- stockhub/src/components/pages/Stocks/StockBuySell.jsx
+++ stockhub/src/components/pages/Stocks/StockBuySell.jsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React ,{ useContext, useState } from 'react';
 import { Button } from '../../ui/Button';
 import './Stock.css';
+import { StockContext } from "./StockContext";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
 
 function StockBuySell() {
+
+	const [currentPrice, setCurrentPrice] = useState(0);
+	const [stock] = useContext(StockContext);
+
+
+	const buyStock = () => {
+			axios
+				.get(
+					`https://finnhub.io/api/v1/quote?symbol=${stock}&token=bu21m3v48v6u9tetnbig`
+					)
+				.then((res) => {
+					setCurrentPrice(res.data.c);
+					axios.post(`http://localhost:8080/buy`, null, {params: {symbol:stock, price:currentPrice }})
+        				.then((response) => {
+							console.log(response);
+				})})}
+
 	return (
 		<div>
 			<div className='stock-button-container'>
                 <div className='button-buy'>
+					<Link to={"/"} onClick={() => buyStock()}>
 				    <Button buttonColor='green'>Buy</Button>
-                </div>
+					</Link>
                 <div className='button-sell'>
 				<Button buttonColor='red'>Sell</Button>
                 </div>
+			</div>
 			</div>
 		</div>
 	);
 }
 
-export default StockBuySell;
+export default StockBuySell
