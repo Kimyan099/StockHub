78e839a8b63a65bdd2dbb2de2061f7ec3bba2345
Bence <borsi92@gmail.com>
2020-11-12 21:47:25 +0100

    added context values and setters for address, phoneNumber and contextNumber

--- stockhub/src/components/pages/ProfilePage/Register/UserContext.jsx
+++ stockhub/src/components/pages/ProfilePage/Register/UserContext.jsx
@@ -8,10 +8,19 @@ export const CurrentUser = (props) => {
   const [contextEmail, setContextEmail] = useState("");
   const [name, setName] = useState("Guest");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [contextAddress, setContextAddress] = useState("")
+  const [contextPhoneNumber, setContextPhoneNumber] = useState("")
+  const [contextMobileNumber, setContextMobileNumber] = useState("")
 
 
   return (
-    <UserContext.Provider value={[name, setName, isLoggedIn, setIsLoggedIn, contextEmail, setContextEmail,]}>
+    <UserContext.Provider value={[
+      name, setName, 
+      isLoggedIn, setIsLoggedIn, 
+      contextEmail, setContextEmail,
+      contextAddress, setContextAddress,
+      contextPhoneNumber, setContextPhoneNumber,
+      contextMobileNumber, setContextMobileNumber]}>
       {props.children}
     </UserContext.Provider>
   );
